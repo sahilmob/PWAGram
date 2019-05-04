0f13303d8eed1ce6b215ac906330df317abe64ed
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-04 15:24:21 +0300

    register fetch event listener

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,8 @@ self.addEventListener("activate", function(event) {
 	console.log("[Service worker] Activating", event);
 	return self.clients.claim();
 });
+
+self.addEventListener("fetch", function(event) {
+	console.log("[Service worker] Fetch", event);
+	event.respondWith(fetch(event.request));
+});
