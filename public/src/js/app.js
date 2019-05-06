6c7785a4edfa7c238faa5e4c8091a06a06ac3af4
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-06 19:00:13 +0300

    catch service worker registration error

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,9 +1,14 @@
 var deferredPrompt;
 
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("/sw.js").then(function() {
-		console.log("Service worker registered");
-	});
+	navigator.serviceWorker
+		.register("/sw.js")
+		.then(function() {
+			console.log("Service worker registered");
+		})
+		.catch(function(err) {
+			console.log(err);
+		});
 }
 
 window.addEventListener("beforeinstallprompt", function(event) {
