556bda7169e82ef533a8f7a7c6f0b3c17e64ec72
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-08 18:12:15 +0300

    cache app.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,11 @@
 self.addEventListener("install", function(event) {
 	console.log("[Service worker] Installing", event);
+	event.waitUntil(
+		caches.open("static").then(function(cache) {
+			console.log("[Service Worker] Precaching App Shell");
+			cache.add("/src/js/app.js");
+		})
+	);
 });
 
 self.addEventListener("activate", function(event) {
