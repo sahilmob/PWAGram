a0f161f4bf7d101feb8fa47f4237ddbd9bd8ba38
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-08 19:14:13 +0300

    remove old cache

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,10 @@
+var CACHE_STATIC_NAME = "static-4";
+var CACHE_DYNAMIC_NAME = "dynamic-v2";
+
 self.addEventListener("install", function(event) {
 	console.log("[Service worker] Installing", event);
 	event.waitUntil(
-		caches.open("static").then(function(cache) {
+		caches.open(CACHE_STATIC_NAME).then(function(cache) {
 			console.log("[Service Worker] Precaching App Shell");
 			cache.addAll([
 				"/",
@@ -24,6 +27,18 @@ self.addEventListener("install", function(event) {
 
 self.addEventListener("activate", function(event) {
 	console.log("[Service worker] Activating", event);
+	event.waitUntil(
+		caches.keys().then(function(keyList) {
+			return Promise.all(
+				keyList.map(function(key) {
+					if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+						console.log("[Service Worker] Removing old cache. ", key);
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
 	return self.clients.claim();
 });
 
@@ -35,7 +50,7 @@ self.addEventListener("fetch", function(event) {
 			} else {
 				return fetch(event.request)
 					.then(function(res) {
-						return caches.open("dynamic").then(function(cache) {
+						return caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
 							cache.put(event.request.url, res.clone());
 							return res;
 						});
