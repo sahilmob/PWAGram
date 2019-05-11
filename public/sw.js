85eb41741e685a44314deda30798d860d004f3f9
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-11 17:21:07 +0300

    implement netwrok with cache fallback

--- public/sw.js
+++ public/sw.js
@@ -43,25 +43,41 @@ self.addEventListener("activate", function(event) {
 	return self.clients.claim();
 });
 
+//Implementing dynamic caching after requesting data from the network
+// self.addEventListener("fetch", function(event) {
+// 	event.respondWith(
+// 		caches.match(event.request).then(function(response) {
+// 			if (response) {
+// 				return response;
+// 			} else {
+// 				return fetch(event.request)
+// 					.then(function(res) {
+// 						return caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+// 							cache.put(event.request.url, res.clone());
+// 							return res;
+// 						});
+// 					})
+// 					.catch(function(err) {
+// 						return caches.open(CACHE_STATIC_NAME).then(function(cache) {
+// 							return cache.match("/offline.html");
+// 						});
+// 					});
+// 			}
+// 		})
+// 	);
+// });
+
 self.addEventListener("fetch", function(event) {
 	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			if (response) {
-				return response;
-			} else {
-				return fetch(event.request)
-					.then(function(res) {
-						return caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
-							cache.put(event.request.url, res.clone());
-							return res;
-						});
-					})
-					.catch(function(err) {
-						return caches.open(CACHE_STATIC_NAME).then(function(cache) {
-							return cache.match("/offline.html");
-						});
-					});
-			}
-		})
+		fetch(event.request)
+			.then(function(res) {
+				return caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+					cache.put(event.request.url, res.clone());
+					return res;
+				});
+			})
+			.catch(function(err) {
+				return caches.match(event.request);
+			})
 	);
 });
