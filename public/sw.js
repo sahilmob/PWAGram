78d3db41b7552c90c5215c8d3b9557d340c94897
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-14 19:09:09 +0300

    add and use idb

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,6 @@
-var CACHE_STATIC_NAME = "static-6";
+importScripts("/src/js/idb.js");
+
+var CACHE_STATIC_NAME = "static-7";
 var CACHE_DYNAMIC_NAME = "dynamic-v5";
 var STATIC_FILES = [
 	"/",
@@ -6,6 +8,7 @@ var STATIC_FILES = [
 	"/offline.html",
 	"/src/js/app.js",
 	"/src/js/feed.js",
+	"/src/js/idb.js",
 	"/src/js/promise.js",
 	"/src/js/fetch.js",
 	"/src/js/material.min.js",
@@ -16,6 +19,12 @@ var STATIC_FILES = [
 	"https://fonts.googleapis.com/icon?family=Material+Icons",
 	"https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
 ];
+
+var dbPromise = idb.open("posts-store", 1, function(db) {
+	if (!db.objectStoreNames.contains("posts")) {
+		db.createObjectStore("posts", { keyPath: "id" });
+	}
+});
 
 function trimCache(cacheName, maxItems) {
 	caches.open(cacheName).then(function(cache) {
@@ -78,12 +87,19 @@ self.addEventListener("fetch", function(event) {
 	var url = "https://pwagram-f1780.firebaseio.com/posts.json";
 	if (event.request.url.indexOf(url) > -1) {
 		event.respondWith(
-			caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
-				fetch(event.request).then(function(response) {
-					// trimCache(CACHE_DYNAMIC_NAME, 3);
-					cache.put(event.request.url, response.clone());
-					return response;
+			fetch(event.request).then(function(res) {
+				var clonedRes = res.clone();
+				clonedRes.json().then(function(data) {
+					for (var key in data) {
+						dbPromise.then(function(db) {
+							var tx = db.transaction("posts", "readwrite");
+							var store = tx.objectStore("posts");
+							store.put(data[key]);
+							return tx.complete;
+						});
+					}
 				});
+				return res;
 			})
 		);
 	} else if (isInArray(event.request.url, STATIC_FILES)) {
