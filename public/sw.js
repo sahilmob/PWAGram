1a7a1e68540b2dd6830b9591c55f7f79cae562dd
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-16 17:05:21 +0300

    fix offline page

--- public/sw.js
+++ public/sw.js
@@ -113,9 +113,7 @@ self.addEventListener("fetch", function(event) {
 						})
 						.catch(function(err) {
 							return caches.open(CACHE_STATIC_NAME).then(function(cache) {
-								if (
-									event.request.url.headers.get("accept").includes("text/html")
-								) {
+								if (event.request.headers.get("accept").includes("text/html")) {
 									return cache.match("/offline.html");
 								}
 							});
